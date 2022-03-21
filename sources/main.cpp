22f139876c220ac1a868bf09fecffbc4c0909243
Breno Souza <cognocoder@gmail.com>
2022-03-21 12:07:29 -0300

    Add Doxygen Doxyfile and main documetation

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,10 @@
 
+///
+/// @file main.cpp
 #include <iostream>
 
+///
+/// Divrer entry point.
 int main() {
   std::cout << "   · int main() " << std::endl;
   std::cout << "   · int main() · return 0 " << std::endl;
